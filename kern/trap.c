0561afea51d0b53e55303e43a569cd598ecfb03e
cyyzero <cyyzero@qq.com>
2022-01-16 03:41:04 +0800

    Trap handler dispatch PageFault

--- kern/trap.c
+++ kern/trap.c
@@ -175,7 +175,11 @@ static void
 trap_dispatch(struct Trapframe *tf)
 {
 	// Handle processor exceptions.
-	// LAB 3: Your code here.
+	switch (tf->tf_trapno) {
+	case T_PGFLT:
+		page_fault_handler(tf);
+		break;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
@@ -235,8 +239,11 @@ page_fault_handler(struct Trapframe *tf)
 	fault_va = rcr2();
 
 	// Handle kernel-mode page faults.
+	if (tf->tf_cs == 0) {
 
-	// LAB 3: Your code here.
+	} else if (tf->tf_cs == 3) {
+
+	}
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
